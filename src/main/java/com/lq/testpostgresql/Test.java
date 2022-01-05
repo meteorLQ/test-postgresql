5e94aca58b07eb92314cdd965a1537d4e2c03f34
shadowLQ <415804410@qq.com>
2022-01-05 18:44:45 +0800

    feat:jooq

--- src/main/java/com/lq/testpostgresql/Test.java
+++ src/main/java/com/lq/testpostgresql/Test.java
@@ -1,11 +1,17 @@
 package com.lq.testpostgresql;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.TransactionManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import test.generated.tables.daos.SysUserDao;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
@@ -16,6 +22,11 @@ import java.util.Map;
 @RequestMapping("/test")
 @Controller
 public class Test {
+    @Autowired
+    DSLContext dslContext;
+
+    @Autowired
+    TransactionManager transactionManager;
     @Resource
     JdbcTemplate jdbcTemplate;
 
@@ -27,6 +38,14 @@ public class Test {
         System.out.println("dataSource.getJdbcUrl() = " + dataSource.getJdbcUrl());
         System.out.println("dataSource.getUsername() = " + dataSource.getUsername());
         return "连接成功！";
+    }
 
+    @GetMapping("/testDsl")
+    @ResponseBody
+    public List testDsl(){
+        SysUserDao sysUserDao = new SysUserDao();
+        List<test.generated.tables.pojos.SysUser> all = sysUserDao.findAll();
+
+        return  all;
     }
 }
